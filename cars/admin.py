d494041f843093eefeaf2d4777baffec1111e94b
EC2 Default User <ec2-user@ip-172-31-0-194.eu-west-1.compute.internal>
2025-04-02 20:57:12 +0000

    third commit

--- cars/admin.py
+++ cars/admin.py
@@ -7,8 +7,8 @@ class CarAdmin(admin.ModelAdmin):
     def images(self, object):
         return format_html('<img src="{}" width="40" style="border-radius: 50px;"/>'.format(object.car_photo.url))
         
-    list_display = ('images', 'car_title', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured', 'city')
-    list_display_links = ('car_title', 'images')
+    list_display = ('id', 'images', 'car_title', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured', 'city')
+    list_display_links = ('id','car_title', 'images')
     list_editable = ('is_featured',)
     search_fields = ('car_title', 'city', 'model', 'year', 'fuel_type')
     list_filter = ('city', 'model')
